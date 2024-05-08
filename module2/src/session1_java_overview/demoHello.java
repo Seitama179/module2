8a1016da692f84b35396b020756c1db6132e1401
Seitama179 <ngochd997@gmail.com>
2024-05-08 15:13:11 +0700

    add thuc hanh

---
+++ module2/src/session1_java_overview/demoHello.java
@@ -0,0 +1,16 @@
+package session1_java_overview;
+
+public class demoHello {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        double c = 7/3;
+        double e = 2.0;
+        System.out.println(c);
+        char text = 'a';
+        String str = "Hello";
+        System.out.println(str);
+        int a1 = 1; // trong phuong thuc khong co gia tri mac dinh, ngoai phuong thuc mac dinh =0
+        System.out.println(a);
+    }
+}
---
+++ untitled/src/hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("What's your name?");
+        Scanner name = new Scanner(System.in);
+        System.out.println(name);
+    }
+}
